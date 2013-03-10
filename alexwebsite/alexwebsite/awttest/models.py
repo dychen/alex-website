c4e544b176a57526c3344bec4ba8a97c7fd12a16
Daniel Chen <dychen@caltech.edu>
2013-03-10 04:58:19 -0700

    Made the models.

--- alexwebsite/alexwebsite/awttest/models.py
+++ alexwebsite/alexwebsite/awttest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TestUser(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Response(models.Model):
+    name = models.ForeignKey(TestUser)
+    question = models.PositiveIntegerField()
+    answer = models.IntegerField()
+    correct = models.NullBooleanField(null=True)
+    
