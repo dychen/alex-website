7db9ae26e49f3ac8ff48e13af4d7f0b8c1d39b3b
Daniel Chen <dychen@caltech.edu>
2013-03-11 04:26:27 -0700

    Added gunicorn to settings.py

--- alexwebsite/alexwebsite/settings.py
+++ alexwebsite/alexwebsite/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'alexwebsite.awttest',
+    'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
