b7ff2e7c2507f93f9bf3b72baedfd8e21d77ecf6
Daniel Chen <dychen@caltech.edu>
2013-03-11 04:51:35 -0700

    Oops again

--- alexwebsite/production_settings.py
+++ alexwebsite/production_settings.py
@@ -3,4 +3,4 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-DATABASE_URL = postgres://zxfsnctqbwuscg:-VwQtETgJwxZGdNX-VwABqGPU3@ec2-23-23-237-0.compute-1.amazonaws.com:5432/d1ria2u0hl68j0
+DATABASE_URL = 'postgres://zxfsnctqbwuscg:-VwQtETgJwxZGdNX-VwABqGPU3@ec2-23-23-237-0.compute-1.amazonaws.com:5432/d1ria2u0hl68j0'
